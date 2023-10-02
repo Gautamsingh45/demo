deedf4e8d587c6c4adc8d1f200405b7ecbec97b4
Gautam singh <79733180+Gautamsingh45@users.noreply.github.com>
2023-10-03 01:03:20 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,7 +16,7 @@ function Login() {
 
         try{
 
-            await axios.post("http://localhost:8000/",{
+            await axios.post("https://frontend2-ym1r.onrender.com/",{
                 email,password
             })
             .then(res=>{
